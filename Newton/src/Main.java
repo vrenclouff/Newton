31217d541dfa4cac578806c0684397582f78e9f7
Lukas Cerny <luc.cerny@gmail.com>
2017-12-27 23:19:35 +0100

    Spusteni programu

--- Newton/src/Main.java
+++ Newton/src/Main.java
@@ -1,20 +1,39 @@
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class Main {
-    private static final String INPUT = "testSimpleInput.txt";
 
-//    private static final String INPUT = "testInput.txt";
+    private static final String FILE_FORMAT = "n";
 
+    @Parameter(required = true, description = "Source file")
+    private String inputFile;
 
-    public static void main(String[]args) {
+    @Parameter(names = {"--output", "-o"})
+    private String outputFile = "a.out";
+
+    public static void main(String... argv) {
+        Main main = new Main();
         try {
-            NewtonLexer lexer = new NewtonLexer(CharStreams.fromFileName(INPUT));
+            JCommander.newBuilder().addObject(main).build().parse(argv);
+            main.run();
+        } catch (ParameterException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void run() {
+        try {
+            validInputFile();
+            NewtonLexer lexer = new NewtonLexer(CharStreams.fromFileName(inputFile));
             CommonTokenStream tokens = new CommonTokenStream(lexer);
 
             NewtonParser parser = new NewtonParser(tokens);
@@ -28,18 +47,31 @@ public class Main {
             printInstructions(visitor);
 
         } catch (IOException e) {
-           System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
         } catch (GrammarException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static void printInstructions(MainVisitor visitor) {
+    private void validInputFile() throws IOException {
+        String[] fileComponents = inputFile.split("\\.");
+        String fileFormat = fileComponents[fileComponents.length -1];
+        if (!fileFormat.equals(FILE_FORMAT)) {
+            throw new IOException("Supported only '."+FILE_FORMAT+"' files.");
+        }
+    }
+
+    private void printInstructions(MainVisitor visitor) {
         Iterator instructions = visitor.getInstructions().iterator();
         int line = 0;
-        System.out.println("----- INSTRUCTIONS ------");
-        while(instructions.hasNext()) {
-            System.out.println((line++) + "\t" + instructions.next());
+        try {
+            FileWriter writer = new FileWriter(outputFile);
+            while (instructions.hasNext()) {
+                writer.write((line++) + "\t" + instructions.next() + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
